ce58c496d4bcb8769a133ebd023a6ce8a07eb683
OniOni <mathieu.c.sabourin@gmail.com>
2014-01-04 16:34:58 +0100

    Save time.

--- server.py
+++ server.py
@@ -3,35 +3,35 @@ app = Flask(__name__)
 
 import redis
 import os
+import time
 
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 r = redis.from_url(redis_url)
 
 @app.route("/app")
-def main():    
+def main():
     url_for('static', filename='app.js')
-    return render_template('index.html', caca=r.get('caca'), pipi=r.get('pipi'))
+    return render_template('index.html', caca=r.llen('caca'), pipi=r.llen('pipi'))
 
+def add_one(value):
+    now = time.time()
+    r.rpush(value, now)
+    return jsonify(**{'result': 'OK', value: r.llen(value)})
 
 @app.route("/pipi")
 def pipi():
-    r.incr('pipi')
-    return jsonify(result='OK', pipi=r.get('pipi'))
-
+    return add_one('pipi')
 
 @app.route("/caca")
 def caca():
-    r.incr('caca')
-    return jsonify(result='OK', caca=r.get('caca'))
-
+    return add_one('caca')
 
 @app.route("/status")
 def status():
-    return jsonify(caca=r.get('caca'), pipi=r.get('pipi'))
+    return jsonify(caca=r.llen('caca'), pipi=r.llen('pipi'))
 
 
 if __name__ == "__main__":
     app.run(
         host='0.0.0.0',
         debug=True)
-    
