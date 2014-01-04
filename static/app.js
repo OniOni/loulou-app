85b90f0103670bba87d10f191ea87406e1c7db30
OniOni <mathieu.c.sabourin@gmail.com>
2014-01-04 18:29:23 +0100

    Add ui for saved times.

--- static/app.js
+++ static/app.js
@@ -1,12 +1,31 @@
 $(function () {
-    $('button').each(function () {
-        var button = $(this);
-        button.click(function () {
-            console.log(button.attr('id'));
-            var name = button.attr('id').split('_')[0];
-            $.getJSON('/'+name, function(data) {
-                $('#'+name).html(data[name]);
-            });
-        });
+  $('button[data-role=incr]').each(function () {
+    var button = $(this);
+    button.click(function () {
+      console.log(button.attr('id'));
+      var name = button.attr('id').split('_')[0];
+      $.getJSON('/'+name, function(data) {
+        $('#'+name).html(data[name]);
+      });
     });
+  });
+
+  $('li').each(function () {
+    var nav = $(this);
+    nav.click(function () {
+      var page = nav.attr('data-dest');
+      console.log("Nav !!! " + page);
+      $("body").pagecontainer("change", $('#list'));
+      $("h1").html(page);
+      $.getJSON('/'+page+'/info', function(data) {
+        var list = $('#info-list');
+        console.log(data);
+
+        for(var i = 0; i < data['times'].length; i++) {
+          list.append('<li class="ui-body-inherit ui-li-static">'+data['times'][i]+"</li>")
+        }
+      })
+
+    });
+  });
 });
